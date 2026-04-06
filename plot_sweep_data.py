Fixed formula for walk operator queries

plot_sweep_data.py
@@ -115,7 +115,7 @@ def plot_total_tcost_comparison(filepath, delta_E=1.0, e=0.1, E_kin=10, Cp=1e-3)
         lam = r_entry['Physical_Lambda']
         
         # Total T-cost = Total_T_Count * (sqrt(2*pi) * Physical_Lambda / delta_E)
-        qpe_walk_queries = (np.sqrt(2 * np.pi) * lam) / delta_E
+        qpe_walk_queries = (np.sqrt(2) * np.pi * lam) / delta_E
         total_qpe = t_step_cost * qpe_walk_queries
         
         qpe_A_vals.append(A)